b6aafbbfa75bb9b39eb5dc156c9dd7bde51d45fd
Jeffrey Yeung <jeffreyysy@gmail.com>
2016-08-09 23:43:37 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+var movementStrength = 20;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("section.module.parallax-1").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 100;
+          var newvalueY = height * pageY * -1 - 25;
+          $('section.module.parallax-1').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
